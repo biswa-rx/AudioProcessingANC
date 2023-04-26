60d40074b1e267031ff7459b2b9514450b26a1bb
biswa <biswaranjanbeheraqazmlp@gmail.com>
2023-04-26 12:22:41 +0530

    Update spectrum graph called before high pass filter and after high pass filter and 2 graph are showing graph... There are high distortion in audio signal after fft... Window function not working properly.

--- app/src/main/java/com/example/spectrumanalyseranc/MainActivity.java
+++ app/src/main/java/com/example/spectrumanalyseranc/MainActivity.java
@@ -5,30 +5,43 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.CatmullRomInterpolator;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private XYPlot spectrumPlot,ancPlot;
+    private XYPlot spectrumPlot, ancPlot;
+//    private Handler mHandler;
     private static final String TAG = "MainActivity";
 
-    int audioSource,sampleRateInHz,channelConfig,audioFormat,bufferSizeInBytes;
-    int streamType,sampleRateOutHz,channelConfigOut,audioFormatOut,bufferSizeOutBytes;
+    int audioSource, sampleRateInHz, channelConfig, audioFormat, bufferSizeInBytes;
+    int streamType, sampleRateOutHz, channelConfigOut, audioFormatOut, bufferSizeOutBytes;
 
-    boolean parameterUpdatedFlag = false,continueStreamMode = true,outputSoundEnable = false;
+    boolean parameterUpdatedFlag = false, continueStreamMode = true, outputSoundEnable = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spectrumPlot = findViewById(R.id.spectrum_plot);
         ancPlot = findViewById(R.id.ANC_plot);
+//        mHandler = new Handler(Looper.getMainLooper());
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -40,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                     channelConfig = AudioFormat.CHANNEL_IN_MONO;
                     audioFormat = AudioFormat.ENCODING_PCM_16BIT;
 //                  int bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat);
-                    bufferSizeInBytes = 16384;
+                    bufferSizeInBytes = 4096;
                     if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
                         return;
                     }
@@ -73,9 +86,11 @@ public class MainActivity extends AppCompatActivity {
                             complexSamples[i] = new Complex(audioSamples[i], 0);
                         }
                         fft(complexSamples, false);
+                        updateSpectrumGraph(complexSamples,spectrumPlot);
                         highPassFilter(complexSamples, 1500, 44100);
+                        updateSpectrumGraph(complexSamples,ancPlot);
                         fft(complexSamples, true);
-                        if(outputSoundEnable) {
+                        if (outputSoundEnable) {
                             byte[] filteredBytes = new byte[2 * complexSamples.length];
                             for (int i = 0; i < complexSamples.length; i++) {
                                 short sample = (short) (complexSamples[i].re * 32767.0);
@@ -91,8 +106,42 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
     }
+    private Complex[] windowFilteredSampleFFT(Complex[] samples){
+        int windowSize = samples.length; // or a power of 2 for better performance
+        double[] window = new double[windowSize];
+        for (int i = 0; i < windowSize; i++) {
+            window[i] = 0.54 - 0.46 * Math.cos(2 * Math.PI * i / (windowSize - 1));
+        }
 
-    static void highPassFilter(Complex[] frequencyDomainSignal,int cutoffFrequency,int sampleRate){
+// Apply the window function to the audio data
+        for (int i = 0; i < windowSize; i++) {
+            samples[i].re = samples[i].re * window[i];
+        }
+        fft(samples,false);
+        return samples;
+    }
+    private void updateSpectrumGraph(Complex[] complexSamples, XYPlot xyPlot) {
+        Number[] frequencyDomain = new Number[complexSamples.length/2];
+        for (int i = 0; i < complexSamples.length/2; i++) {
+//            short sample = (short) (Math.sqrt((complexSamples[i].re * complexSamples[i].re) + (complexSamples[i].im * complexSamples[i].im)) * 32767.0);
+            short sample = (short) (complexSamples[i].re * 2000.0);
+            frequencyDomain[i] = sample;
+        }
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(frequencyDomain), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.RED, null, null, null);
+        series1Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+        xyPlot.setDomainBoundaries(0, frequencyDomain.length, BoundaryMode.AUTO);
+        xyPlot.setRangeBoundaries(Short.MIN_VALUE,Short.MAX_VALUE,BoundaryMode.AUTO);
+        xyPlot.clear();
+        xyPlot.addSeries(series1, series1Format);
+        Log.d(TAG, "run: running updateSpectrum");
+        xyPlot.redraw();
+
+    }
+
+    static void highPassFilter(Complex[] frequencyDomainSignal, int cutoffFrequency, int sampleRate) {
         int numSamples = frequencyDomainSignal.length;
         int numFrequencies = numSamples / 2 + 1;
         int cutoffIndex = cutoffFrequency * numSamples / sampleRate;
@@ -101,17 +150,39 @@ public class MainActivity extends AppCompatActivity {
             frequencyDomainSignal[numSamples - i] = Complex.ZERO;
         }
     }
+
     static class Complex {
         double re, im;
         public static final Complex ZERO = new Complex(0, 0);
-        Complex(double x, double y) { re = x; im = y; }
-        Complex() { this(0, 0); }
-        Complex add(Complex b) { return new Complex(re+b.re, im+b.im); }
-        Complex sub(Complex b) { return new Complex(re-b.re, im-b.im); }
-        Complex mul(Complex b) { return new Complex(re*b.re - im*b.im, re*b.im + im*b.re); }
-        Complex div(double x) { return new Complex(re/x, im/x); }
+
+        Complex(double x, double y) {
+            re = x;
+            im = y;
+        }
+
+        Complex() {
+            this(0, 0);
+        }
+
+        Complex add(Complex b) {
+            return new Complex(re + b.re, im + b.im);
+        }
+
+        Complex sub(Complex b) {
+            return new Complex(re - b.re, im - b.im);
+        }
+
+        Complex mul(Complex b) {
+            return new Complex(re * b.re - im * b.im, re * b.im + im * b.re);
+        }
+
+        Complex div(double x) {
+            return new Complex(re / x, im / x);
+        }
     }
+
     static final double PI = Math.acos(-1);
+
     static void fft(Complex[] a, boolean invert) {
         int n = a.length;
         if (n == 1)
@@ -120,8 +191,8 @@ public class MainActivity extends AppCompatActivity {
         Complex[] a0 = new Complex[n / 2];
         Complex[] a1 = new Complex[n / 2];
         for (int i = 0; 2 * i < n; i++) {
-            a0[i] = a[2*i];
-            a1[i] = a[2*i+1];
+            a0[i] = a[2 * i];
+            a1[i] = a[2 * i + 1];
         }
         fft(a0, invert);
         fft(a1, invert);
@@ -131,12 +202,17 @@ public class MainActivity extends AppCompatActivity {
         Complex wn = new Complex(Math.cos(ang), Math.sin(ang));
         for (int i = 0; 2 * i < n; i++) {
             a[i] = a0[i].add(w.mul(a1[i]));
-            a[i + n/2] = a0[i].sub(w.mul(a1[i]));
+            a[i + n / 2] = a0[i].sub(w.mul(a1[i]));
             if (invert) {
                 a[i] = a[i].div(2);
-                a[i + n/2] = a[i + n/2].div(2);
+                a[i + n / 2] = a[i + n / 2].div(2);
             }
             w = w.mul(wn);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
